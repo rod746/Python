6313da90a714c161ee631a01dd915dcde00ab6ee
rod <dolfo.nf@gmail.com>
2025-05-17 17:10:26 -0300

    Create testes básicos.py

---
+++ testes básicos.py
@@ -0,0 +1,33 @@
+#TESTE COM NOÇÕES BÁSICAS EM PYTHON
+#IMPRIMINDO COM PRINT()
+print("teste1"); print("teste2"); print("teste3")
+#TIPOS DE DADOS
+inteiro = 2
+float = 2.1
+string = "rods"
+booleano = True, False
+#OPERADORES ARITMÉTICOS
+'''Os operadores aritméticos são utilizados para realizar operações matemáticas básicas. Os principais operadores aritméticos em Python são:
+
+Soma (+): soma dois valores.
+Subtração (-): subtrai o segundo valor do primeiro.
+Multiplicação (*): multiplica dois valores.
+Divisão (/): divide o primeiro valor pelo segundo e devolve um resultado de tipo flutuante.
+Divisão inteira (//): divide o primeiro valor pelo segundo e devolve um resultado de tipo inteiro (a parte decimal é descartada).
+Módulo (%): devolve o resto da divisão entre o primeiro valor e o segundo.
+Exponenciação (**): eleva o primeiro valor à potência do segundo.'''
+#DE COMPARAÇÃO
+'''Os operadores de comparação são utilizados para comparar dois valores e devolvem um valor booleano (True ou False) segundo o resultado da comparação. Os operadores de comparação em Python são:
+
+Igual a (==): devolve True se ambos os valores são iguais.
+Diferente de (!=): devolve True se os valores são diferentes.
+Maior que (>): devolve True se o primeiro valor é maior que o segundo.
+Menor que (<): devolve True se o primeiro valor é menor que o segundo.
+Maior ou igual que (>=): devolve True se o primeiro valor é maior ou igual que o segundo.
+Menor ou igual que (<=): devolve True se o primeiro valor é menor ou igual que o segundo.'''
+#LÓGICOS
+'''Os operadores lógicos são utilizados para combinar expressões condicionais e avaliar múltiplas condições. Os operadores lógicos em Python são:
+
+AND (and): devolve True se ambas as condições são verdadeiras.
+OR (or): devolve True se ao menos uma das condições é verdadeira.
+NOT (not): inverte o valor de uma condição, devolve True se a condição é falsa e False se a condição é verdadeira.'''
